084c8556f6a7eb88ed942888bfc7ccbc00db4483
Charles BARDIN <charles.bardin.pro@gmail.com>
2017-09-22 12:33:56 +0200

    Application part implementation

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'
+import { MdListModule, MdButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
+
+import { SharedModule } from './shared/shared.module'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+
+    MdListModule,
+    MdButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
